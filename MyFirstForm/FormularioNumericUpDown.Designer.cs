899de1f351767f4122a206092ad716efacd4e153
Moisés Navarro <moi55navacor@gmail.com>
2021-03-19 20:31:42 -0600

    Added frmNumericUpDown

---
+++ MyFirstForm/FormularioNumericUpDown.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace MyFirstForm
+{
+    partial class FormularioNumericUpDown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbValor = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.NumericUpDown();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lbMostrar = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.txtValor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbValor
+            // 
+            this.lbValor.AutoSize = true;
+            this.lbValor.Location = new System.Drawing.Point(12, 28);
+            this.lbValor.Name = "lbValor";
+            this.lbValor.Size = new System.Drawing.Size(34, 13);
+            this.lbValor.TabIndex = 0;
+            this.lbValor.Text = "Valor:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(52, 28);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(138, 20);
+            this.txtValor.TabIndex = 1;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(197, 28);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 2;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lbMostrar
+            // 
+            this.lbMostrar.AutoSize = true;
+            this.lbMostrar.Location = new System.Drawing.Point(49, 60);
+            this.lbMostrar.Name = "lbMostrar";
+            this.lbMostrar.Size = new System.Drawing.Size(0, 13);
+            this.lbMostrar.TabIndex = 3;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(278, 28);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // FormularioNumericUpDown
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(364, 108);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lbMostrar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.lbValor);
+            this.Name = "FormularioNumericUpDown";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Numeric";
+            ((System.ComponentModel.ISupportInitialize)(this.txtValor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbValor;
+        private System.Windows.Forms.NumericUpDown txtValor;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Label lbMostrar;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
---
+++ MyFirstForm/FormularioNumericUpDown.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstForm
+{
+    public partial class FormularioNumericUpDown : Form
+    {
+        public FormularioNumericUpDown()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            decimal valor = txtValor.Value;
+
+            lbMostrar.Text = "El valor es: " + valor;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lbMostrar.Text = "";
+            txtValor.Value = 0;
+        }
+    }
+}
